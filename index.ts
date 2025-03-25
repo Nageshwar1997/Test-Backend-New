4f1546b4616cfff84a693c7c711d792c7ed7beea
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 12:23:33 +0530

    Test

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
