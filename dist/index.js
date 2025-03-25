700310ae0cdb5a475b51899aab9a6c39a8518f15
nageshwarp1997 <nageshwar@ctruh.com>
2025-03-25 13:48:36 +0530

    `Update dependencies and configuration for TypeScript project`
    `diff` shows changes to `package-lock.json`, `package.json`, `src/index.ts`, and `tsconfig.json` files.

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,10 @@
 import express from "express";
-
 const app = express();
-
 const PORT = 8080;
 app.get("/", (req, res) => {
-  const headers = req.headers;
-  res.status(200).send("Hello world!");
+    const headers = req.headers;
+    res.status(200).send("Hello world!");
 });
-
 app.listen(PORT, () => {
-  console.log("Server listening on port", PORT);
+    console.log("Server listening on port", PORT);
 });
